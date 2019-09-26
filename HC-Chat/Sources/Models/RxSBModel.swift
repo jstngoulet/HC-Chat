5a912654376b782dfd3fe2c776d2782765133982
Tizzle Goulet <jstngoulet@me.com>
2019-09-25 22:22:31 -0700

    Updated target membership for core data model

--- HC-Chat/Sources/Models/RxSBModel.swift
+++ HC-Chat/Sources/Models/RxSBModel.swift
@@ -16,69 +16,30 @@ open class RxSBModel: NSObject {
     // MARK: - Core Data stack
     
     fileprivate lazy var managedObjectModel: NSManagedObjectModel? = {
-        
-        // the moc's model should be accessible via apps in this workspace
-        // or through the module that cocoapods will create as part of the file's
-        // resource bundle, as such, we need to look in two different places
-        // to use the correct bundle at run time
-        var rawBundle: Bundle? {
-            
-            if let bundle = Bundle(identifier: "com.hyrecar.ios.pod.RxSBChat") {
-                return bundle
-            }
-            
-            if let podBundleURL = Bundle(for: RxSBModel.self).url(forResource: "RxSBChat", withExtension: "bundle"),
-                let frameworkBundle = Bundle(url: podBundleURL) {
-                print("Found Framework Bundle :\(podBundleURL.absoluteString)")
-                return frameworkBundle
-            }
-            
-//            print("Frameworks: \(Bundle.allFrameworks.compactMap({ $0.bundleURL }))\n\nBundles: \(Bundle.allBundles.compactMap({ $0.bundleURL}))")
-
-            guard
-                let resourceBundleURL = Bundle(for: type(of: self)).url(forResource: "RxSBChat", withExtension: "bundle"),
-                let realBundle = Bundle(url: resourceBundleURL) else {
-                    return nil
-            }
-            
-            return realBundle
-        }
-        
-        guard let bundle: Bundle = rawBundle else {
-            return nil
-        }
-        
-        guard let modelURL = bundle.url(forResource: "RxSBChatModels", withExtension: "momd"),
-            let mom = NSManagedObjectModel(contentsOf: modelURL)
-            else { print("Could not get local model"); return nil }
-        
-        return mom
+        return NSManagedObjectModel.mergedModel(from: [Bundle.main])
     }()
     
     lazy var persistentContainer: NSPersistentContainer = {
-        guard let objectModelFound = managedObjectModel else {
-            //  No model found, look in App Delegate
-            return chatPersistentContainer
-        }
-        let container = NSPersistentContainer(name: "main", managedObjectModel: objectModelFound)
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let errorFound = error {
-                print("Error found on container load: \(errorFound)")
-                return
-            }
-            print("Store Description: \(storeDescription)")
-        })
-        return container
-    }()
-    
-    lazy var chatPersistentContainer: NSPersistentContainer = {
         /*
          The persistent container for the application. This implementation
          creates and returns a container, having loaded the store for the
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
          */
-        let container = NSPersistentContainer(name: "RxSBChatModels")
+        //...
+        let momdName = "HC-ChatModels" //pass this as a parameter
+        //...
+
+        guard let modelURL = Bundle(for: type(of: self)).url(forResource: momdName, withExtension:"momd") else {
+                fatalError("Error loading model from bundle")
+        }
+
+        guard let mom = NSManagedObjectModel(contentsOf: modelURL) else {
+            fatalError("Error initializing mom from: \(modelURL)")
+        }
+
+        let container = NSPersistentContainer(name: momdName, managedObjectModel: mom)
+
         container.loadPersistentStores(completionHandler: { (_, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
