c11ec759c697a0e5121ca50e4d2e99a749d80eb4
Tizzle Goulet <jstngoulet@me.com>
2019-09-25 19:53:14 -0700

    Initial Commit

---
+++ HC-Chat/HC_Chat.h
@@ -0,0 +1,19 @@
+//
+//  HC_Chat.h
+//  HC-Chat
+//
+//  Created by Justin Goulet on 9/25/19.
+//  Copyright © 2019 Justin Goulet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HC_Chat.
+FOUNDATION_EXPORT double HC_ChatVersionNumber;
+
+//! Project version string for HC_Chat.
+FOUNDATION_EXPORT const unsigned char HC_ChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HC_Chat/PublicHeader.h>
+
+
